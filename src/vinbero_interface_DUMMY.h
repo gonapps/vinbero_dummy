a23d31805183b7d13b729e0606abb5e16ce6607f
Byeonggon Lee <gonny952@gmail.com>
2018-06-15 19:42:25 +0900

    Interface -> interface

--- src/vinbero_interface_DUMMY.h
+++ src/vinbero_interface_DUMMY.h
@@ -4,18 +4,18 @@
 #include <vinbero_common/vinbero_common_Module.h>
 
 #define VINBERO_INTERFACE_DUMMY_FUNCTIONS \
-int vinbero_Interface_DUMMY_service(struct vinbero_common_Module* module)
+int vinbero_interface_DUMMY_service(struct vinbero_common_Module* module)
 
 #define VINBERO_INTERFACE_DUMMY_FUNCTION_POINTERS \
-int (*vinbero_Interface_DUMMY_service)(struct vinbero_common_Module*)
+int (*vinbero_interface_DUMMY_service)(struct vinbero_common_Module*)
 
-struct vinbero_Interface_DUMMY {
+struct vinbero_interface_DUMMY {
     VINBERO_INTERFACE_DUMMY_FUNCTION_POINTERS;
 };
 
 #define VINBERO_INTERFACE_DUMMY_DLSYM(interface, dlHandle, ret) \
 do { \
-    VINBERO_COMMON_MODULE_DLSYM(interface, dlHandle, vinbero_Interface_DUMMY_service, ret); \
+    VINBERO_COMMON_MODULE_DLSYM(interface, dlHandle, vinbero_interface_DUMMY_service, ret); \
     if(*(ret) < 0) break; \
 } while(0)
 
